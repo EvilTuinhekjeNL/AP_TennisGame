19f9e8861ae4d5d9f7ce705f356d8b98597a652a
EvilTuinhekjeNL <robuithol+github@gmail.com>
2013-03-13 16:54:18 +0100

    Adding test data (setUp() and score())

--- src/TennisGame/TennisGame.java
+++ src/TennisGame/TennisGame.java
@@ -1,5 +1,14 @@
 package TennisGame;
 
 public class TennisGame {
-
+	private int player1, player2;
+	
+	public void setUp(){
+		player1 = 0;
+		player2 = 0;
+	}
+	
+	public int[] score(){
+		return new int[]{ player1, player2 };
+	}
 }
--- src/Test/TennisGameTest.java
+++ src/Test/TennisGameTest.java
@@ -1,9 +1,12 @@
 package Test;
 
+import TennisGame.TennisGame;
 import junit.framework.TestCase;
 
 public class TennisGameTest extends TestCase{
 	public void testTennisGame() throws Exception{
 		TennisGame game = new TennisGame();
+		game.setUp();
+		assertEquals(new int[]{0,0}, game.score());
 	}
 }
