fc70079e2e7902f499ddeb2f9015c4914b265d60
Tim Bazuin <krageon@gmail.com>
2013-03-14 14:37:49 +0100

    pretties

--- src/Test/TennisGameTest.java
+++ src/Test/TennisGameTest.java
@@ -4,30 +4,27 @@ import junit.framework.TestCase;
 import TennisGame.*;
 
 public class TennisGameTest extends TestCase{
+	public void testTennisGame() throws Exception{
+		TennisGame game = setup();
+		assert(evaluateScore(game, 0, 0));
+	}
 	
-	public TennisGame testSetup() throws Exception{
+	public void testTennisScore() throws Exception{
+		TennisGame game = setup();
+		game.score("sideA");
+		game.score("sideA");
+		game.score("sideB");
+		evaluateScore(game, 30, 15);
+	}
+	
+	public TennisGame setup() throws Exception{
 		TennisGame game = new TennisGame();
 		game.setUp();
 		return game;
 	}
 	
-	public void testTennisGame() throws Exception{
-		TennisGame game = testSetup();
-		assert(testEvaluateScore(game, 0, 0));
-	}
-	
-	public void testTennisScore() throws Exception{
-		TennisGame game = testSetup();
-		game.score("sideA");
-		game.score("sideA");
-		game.score("sideB");
-		testEvaluateScore(game, 30, 15);
-	}
-	
-	public boolean testEvaluateScore(TennisGame game, int expectedSideA, int expectedSideB) throws Exception{
+	public boolean evaluateScore(TennisGame game, int expectedSideA, int expectedSideB) throws Exception{
 		int[] score = game.getScore();
 		return score[0] == expectedSideA && score[1] == expectedSideB;
 	}
-	
-
 }
