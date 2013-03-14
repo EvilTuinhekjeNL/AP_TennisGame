93cca31f1b85264923f2cf25305aa4c644f63fbc
Tim Bazuin <krageon@gmail.com>
2013-03-14 14:45:12 +0100

    refactored to JUnit 4

--- src/Test/TennisGameTest.java
+++ src/Test/TennisGameTest.java
@@ -1,26 +1,35 @@
 package Test;
 
-import junit.framework.TestCase;
-import TennisGame.*;
+import org.junit.*;
 
-public class TennisGameTest extends TestCase{
+import TennisGame.TennisGame;
+
+public class TennisGameTest {
+	TennisGame game;
+	
+	@Before
+	public void setup() throws Exception{
+		game = new TennisGame();
+		game.setUp();
+	}
+	
+	@Test
 	public void testTennisGame() throws Exception{
-		TennisGame game = setup();
+		//TennisGame game = setup();
 		assert(evaluateScore(game, 0, 0));
 	}
 	
+	@Test
 	public void testTennisScore() throws Exception{
-		TennisGame game = setup();
+		//TennisGame game = setup();
 		game.score("sideA");
 		game.score("sideA");
 		game.score("sideB");
 		evaluateScore(game, 30, 15);
 	}
 	
-	public TennisGame setup() throws Exception{
-		TennisGame game = new TennisGame();
-		game.setUp();
-		return game;
+	public void testMatchPoint() throws Exception {
+		
 	}
 	
 	public boolean evaluateScore(TennisGame game, int expectedSideA, int expectedSideB) throws Exception{
